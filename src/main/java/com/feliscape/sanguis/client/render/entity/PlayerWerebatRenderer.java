dd62d2ac5749599472f7021fb4d5e22952df5dc1
LittleEzra <natti.adamus@gmail.com>
2026-03-04 20:59:24 +0100

    Started work on werebat textures

--- src/main/java/com/feliscape/sanguis/client/render/entity/PlayerWerebatRenderer.java
+++ src/main/java/com/feliscape/sanguis/client/render/entity/PlayerWerebatRenderer.java
@@ -4,12 +4,18 @@ import com.feliscape.sanguis.Sanguis;
 import com.feliscape.sanguis.client.SanguisModelLayers;
 import com.feliscape.sanguis.client.model.PlayerWerebatModel;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.HumanoidArm;
+import net.neoforged.neoforge.client.ClientHooks;
 
 public class PlayerWerebatRenderer extends PlayerRenderer {
     private static final ResourceLocation WIDE_TEXTURE = Sanguis.location("textures/entity/werebat/wide.png");
@@ -24,6 +30,34 @@ public class PlayerWerebatRenderer extends PlayerRenderer {
                 context.bakeLayer(useSlimModel ? SanguisModelLayers.WEREBAT_SLIM : SanguisModelLayers.WEREBAT_WIDE),
                 useSlimModel
         );
+    }
+
+    public void renderRightHand(PoseStack poseStack, MultiBufferSource buffer, int combinedLight, AbstractClientPlayer player) {
+        if (!ClientHooks.renderSpecificFirstPersonArm(poseStack, buffer, combinedLight, player, HumanoidArm.RIGHT)) {
+            this.renderHand(poseStack, buffer, combinedLight, player, this.model.rightArm, this.model.rightSleeve);
+        }
+
+    }
+
+    public void renderLeftHand(PoseStack poseStack, MultiBufferSource buffer, int combinedLight, AbstractClientPlayer player) {
+        if (!ClientHooks.renderSpecificFirstPersonArm(poseStack, buffer, combinedLight, player, HumanoidArm.LEFT)) {
+            this.renderHand(poseStack, buffer, combinedLight, player, this.model.leftArm, this.model.leftSleeve);
+        }
+
+    }
+
+    private void renderHand(PoseStack poseStack, MultiBufferSource buffer, int combinedLight, AbstractClientPlayer player, ModelPart rendererArm, ModelPart rendererArmwear) {
+        PlayerModel<AbstractClientPlayer> playermodel = this.getModel();
+        this.setModelProperties(player);
+        playermodel.attackTime = 0.0F;
+        playermodel.crouching = false;
+        playermodel.swimAmount = 0.0F;
+        playermodel.setupAnim(player, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+        rendererArm.xRot = 0.0F;
+        ResourceLocation texture = getTextureLocation(player);
+        rendererArm.render(poseStack, buffer.getBuffer(RenderType.entitySolid(texture)), combinedLight, OverlayTexture.NO_OVERLAY);
+        rendererArmwear.xRot = 0.0F;
+        rendererArmwear.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(texture)), combinedLight, OverlayTexture.NO_OVERLAY);
     }
 
     @Override
